15ad0c28416a135883326a83c7e59bae57095333
George <106427405+6ogo@users.noreply.github.com>
2024-11-28 05:53:47 +0100

    should be OK now

--- frontend/src/components/WalletConnect.jsx
+++ frontend/src/components/WalletConnect.jsx
@@ -1,44 +1,70 @@
-// frontend/src/components/WalletConnect.jsx
-import React, { useMemo } from 'react';
-import {
-    ConnectionProvider,
-    WalletProvider,
-    useWallet
-} from '@solana/wallet-adapter-react';
-import {
-    WalletModalProvider,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import {
-    PhantomWalletAdapter,
-    SolflareWalletAdapter,
-    BackpackWalletAdapter
-} from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
-require('@solana/wallet-adapter-react-ui/styles.css');
-
-function WalletConnect() {
-    const wallets = useMemo(
-        () => [
-            new PhantomWalletAdapter(),
-            new SolflareWalletAdapter(),
-            new BackpackWalletAdapter(),
-        ],
-        []
-    );
-
-    return (
-        <ConnectionProvider endpoint={clusterApiUrl('mainnet-beta')}>
-            <WalletProvider wallets={wallets} autoConnect>
-                <WalletModalProvider>
-                    <div className="wallet-container">
-                        <WalletMultiButton />
-                    </div>
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    );
-}
-
-export default WalletConnect;
+module.exports = {
+    entry: './src/components/WalletConnect.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',  // Use content hash for cache busting
+        clean: true  // Clean the dist folder before each build
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "zlib": require.resolve("browserify-zlib"),
+            "buffer": require.resolve("buffer/"),
+            "vm": require.resolve("vm-browserify")
+        }
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        },
+        runtimeChunk: 'single'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+            process: 'process/browser'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ],
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    }
+};
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,39 +1,36 @@
-// frontend/webpack.config.js
 const path = require('path');
 
 module.exports = {
-    entry: './src/components/WalletConnect.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        library: {
-            name: 'WalletConnect',
-            type: 'umd',
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM'
+  entry: './src/index.js', // Adjust this to your entry point
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js', // This ensures unique filenames
+    clean: true // Cleans the dist folder before each build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
     }
+  }
 };
